5f0f53fe72c204351c2696adf60cddaea526bdad
Will <48894810+Twillij@users.noreply.github.com>
2023-07-23 22:35:12 +1000

    Initial commit

---
+++ Source/UnrealShooterGame/UnrealShooterGame.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UnrealShooterGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealShooterGame, "UnrealShooterGame" );
